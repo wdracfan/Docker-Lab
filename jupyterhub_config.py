a196fdb060a3d19e41666be4df8658e49fb23320
Vlad Pichugin <121407222+wdracfan@users.noreply.github.com>
2023-01-01 20:58:56 +0300

    A little more cleanup

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,6 +1,7 @@
 c.JupyterHub.port = 80
 
-c.JupyterHub.authenticator_class = 'jupyterhub.auth.DummyAuthenticator'
+from jupyterhub.auth import DummyAuthenticator
+c.JupyterHub.authenticator_class = DummyAuthenticator
 c.DummyAuthenticator.admin_users = {'admin'}
 c.DummyAuthenticator.allowed_users = {'user'}
 c.DummyAuthenticator.password = 'admin'
